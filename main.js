0dddd1391c3044870d5b63d2e10519970dca3fa7
Jonas Holbech <jh@jonasholbech.dk>
2020-03-05 09:58:42 +0100

    creating branch from hub

---
+++ main.js
@@ -0,0 +1,28 @@
+const genericCatch = error => {
+  const content = document.getElementById("content");
+  content.innerHTML = error;
+};
+const listRepos = async username => {
+  const repos = await fetch(
+    `https://api.github.com/users/${username}/repos?type=owner&sort=updated`
+  )
+    .then(res => res.json())
+    .catch(error => genericCatch);
+
+  const markup = repos
+    .map(
+      repo => `
+        <li>
+          <a href="${repo.html_url}">${repo.name}</a>
+          (⭐️ ${repo.stargazers_count})
+        </li>
+      `
+    )
+    .join("");
+
+  const content = document.getElementById("content");
+
+  content.innerHTML = `<ul>${markup}</ul>`;
+};
+
+listRepos("jonasholbech");
